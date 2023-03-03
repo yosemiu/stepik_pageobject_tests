4d49ab204a78b15a7e94a5beac22572a5a72546a
yosemiu <118342163+yosemiu@users.noreply.github.com>
2023-03-03 11:57:37 +0400

    Add files via upload
    add locators n fix login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
-from .base_page import BasePage
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -7,14 +8,20 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
+# реализуйте проверку на корректный url адрес
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "Not login url"
 
+# реализуйте проверку, что есть форма логина
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
+# реализуйте проверку, что есть форма регистрации на странице
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+
+
+#(*LoginPageLocators.LOGIN_FORM)
+#(*LoginPageLocators.REGISTER_FORM)
+#(*MainPageLocators.LOGIN_LINK)
