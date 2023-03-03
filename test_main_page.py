abaff3fcd3cadc099a2885074502fcfa1d161c0c
yosemiu <118342163+yosemiu@users.noreply.github.com>
2023-03-03 11:58:53 +0400

    Add files via upload
    fix test-main_page(add test_login_url, test_login_n_refister_forms)

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,4 +9,24 @@ def test_guest_can_go_to_login_page(browser):
  # открываем страницу
     page.open()                      
  # выполняем метод страницы — переходим на страницу логина
-    page.go_to_login_page()     
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()  
+
+# создаем тест на корректность урла логина
+def test_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()  
+ 
+# создаем тесты на формы авторизации и регистрации
+def test_login_n_refister_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()  
