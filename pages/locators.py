b1b1e852f7f244405f335bb307926c095ab7ff57
yosemiu <118342163+yosemiu@users.noreply.github.com>
2023-03-06 10:56:03 +0400

    Add files via upload
    fix locators - add new

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "form#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "form#register_form")
+
+
+class ProductPageLocators():
+    BUTTON_ADD = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, "div.col-sm-6.product_main>h1")
+    BOOK_NAME_SMALL = (By.CSS_SELECTOR, "div:first-child>.alertinner>strong")
+    BOOK_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    BOOK_PRICE_SMALL = (By.CSS_SELECTOR, "div.alertinner>p>strong")
